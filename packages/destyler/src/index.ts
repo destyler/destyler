712438287b4089d7d356dabf993d1bae79d9d6cf
elonehoo <hi@elonehoo.me>
2023-09-03 14:11:04 +0800

    feat: add auto import support

--- packages/destyler/src/index.ts
+++ packages/destyler/src/index.ts
@@ -1,8 +1,10 @@
 import { DestylerUIResolver } from './resolver'
+import { DestylerPresets } from './presets'
 
 export * from './components'
 export * from './composition'
 
 export {
   DestylerUIResolver,
+  DestylerPresets,
 }
---
+++ packages/destyler/src/presets.ts
@@ -0,0 +1,8 @@
+import { useAccordionSelected } from '@destyler/accordion'
+
+export const DestylerPresets = [
+  {
+    from: 'destyler',
+    imports: useAccordionSelected,
+  },
+]
