1904d52939c475bbdffe50a6fa3c230af1492203
elonehoo <hi@elonehoo.me>
2023-10-18 18:58:24 +0800

    docs: update button demo

--- docs/.vitepress/components/Button.vue
+++ docs/.vitepress/components/Button.vue
@@ -51,3 +51,31 @@ function handleClick() {
   transition: color .25s,border-color .25s,background-color .25s;
 }
 </style>
+
+<style>
+.fade-in-width-expand-transition-leave-from, .fade-in-width-expand-transition-enter-to{
+  opacity: 1
+}
+
+.fade-in-width-expand-transition-leave-to, .fade-in-width-expand-transition-enter-from{
+  opacity: 0!important;
+  margin-left: 0!important;
+  margin-right: 0!important;
+}
+.fade-in-width-expand-transition-leave-active{
+  overflow: hidden;
+  transition:
+    opacity 0.2s cubic-bezier(.4, 0, .2, 1),
+    max-width 0.2s cubic-bezier(.4, 0, .2, 1) 0.1s,
+    margin-left 0.2s cubic-bezier(.4, 0, .2, 1) 0.1s,
+    margin-right 0.2s cubic-bezier(.4, 0, .2, 1) 0.1s;
+}
+.fade-in-width-expand-transition-enter-active{
+  overflow: hidden;
+  transition:
+    opacity 0.2s cubic-bezier(.4, 0, .2, 1) 0.1s,
+    max-width 0.2s cubic-bezier(.4, 0, .2, 1),
+    margin-left 0.2s cubic-bezier(.4, 0, .2, 1),
+    margin-right 0.2s cubic-bezier(.4, 0, .2, 1);
+}
+</style>
