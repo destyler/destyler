14d4a37cb296282f2445e9698a1d13fc0ecee0b0
elonehoo <hi@elonehoo.me>
2023-10-19 21:12:00 +0800

    test: add avatar test

---
+++ packages/components/avatar/__test__/avatar.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { DestylerAvatar } from '../src'
+
+describe('Avatar', () => {
+  it('image avatar', () => {
+    const wrapper = mount(DestylerAvatar, {
+      props: {
+        src: 'https://github.com/elonehoo.png',
+      },
+    })
+    expect(wrapper.find('img').exists()).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.unmount()
+  })
+  it('image avatar error handle when load failed', async () => {
+    const onError = vi.fn()
+    const wrapper = mount(DestylerAvatar, {
+      props: {
+        src: 'https://github.com/elonehoo.png',
+        onError,
+      },
+    })
+    await wrapper.find('img').trigger('error')
+    expect(onError).toHaveBeenCalled()
+    wrapper.unmount()
+  })
+})
