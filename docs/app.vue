2a58851a4d03a1cc34022b57967e815269eea224
elonehoo <hi@elonehoo.me>
2023-12-31 01:06:37 +0800

    docs(feat): add new logo

--- docs/app.vue
+++ docs/app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: 'Destyler',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/logo.svg',
+    },
+  ],
+})
+</script>
+
 <template>
   <div class="w-100vw h-100vh">
     <NuxtPage />
