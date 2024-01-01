7ce1808be07b6ddde1c195ad26461eed0736bc82
elonehoo <hi@elonehoo.me>
2024-01-01 21:30:22 +0800

    fix: update blockquote max width

--- docs/components/content/prose/ProseBlockquote.vue
+++ docs/components/content/prose/ProseBlockquote.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="text-lg text-#09090b dark:text-#fafafa text-op-60! mt-2">
-    <span style="display: inline-block; vertical-align: top; text-decoration: inherit; max-width: 358px;">
+    <span class="inline-block max-w-full [text-decoration:inherit] [vertical-align:top]">
       <slot />
     </span>
   </p>
