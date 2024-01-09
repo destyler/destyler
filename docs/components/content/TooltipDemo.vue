bd17f731480b2f2402906afa798f5d761922bd2a
elonehoo <hi@elonehoo.me>
2024-01-09 17:28:32 +0800

    fix: conflicts with popover styles

--- docs/components/content/TooltipDemo.vue
+++ docs/components/content/TooltipDemo.vue
@@ -3,11 +3,37 @@ import { DestylerTooltip } from '@destyler/tooltip/src/tooltip'
 </script>
 
 <template>
-  <DestylerTooltip trigger="click">
-    <template #trigger>
-      <ButtonDemo> Duck </ButtonDemo>
-    </template>
-    If it looks like a duck, walks like a duck, and quacks like a duck...it must
-    be a duck.
-  </DestylerTooltip>
+  <ClientOnly>
+    <DestylerTooltip trigger="hover">
+      <template #trigger>
+        <ButtonDemo>Hover</ButtonDemo>
+      </template>
+      Add to library
+    </DestylerTooltip>
+  </ClientOnly>
 </template>
+
+<style>
+[destyler="tooltip-body-toggle"]{
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  transform: translate(calc(var(--destyler-follower-x) - 2rem), calc(var(--destyler-follower-y) - var(--destyler-follower-height) - 1.75rem));
+  min-width: max-content;
+  border-width: 1px;
+  border-radius: 0.375rem;
+  width: 8rem;
+  color:#09090b;
+  padding: 1rem;
+  --un-shadow: var(--un-shadow-inset) 0 4px 6px -1px var(--un-shadow-color, rgb(0 0 0 / 0.1)), var(--un-shadow-inset) 0 2px 4px -2px var(--un-shadow-color, rgb(0 0 0 / 0.1));
+  box-shadow: var(--un-ring-offset-shadow), var(--un-ring-shadow), var(--un-shadow);
+  gap: 1rem;
+  display: grid;
+  background-color: white;
+}
+.dark [destyler="tooltip-body-toggle"]{
+  color:#fafafa;
+  border-color: #27272a;
+  background-color: black;
+}
+</style>
