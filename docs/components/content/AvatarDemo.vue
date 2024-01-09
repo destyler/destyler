05fd22b998dae064d9a3eaba326775bc8c0678cb
elonehoo <hi@elonehoo.me>
2024-01-09 14:56:29 +0800

    fix: resizeObserver component not working in nuxt, added docs preview for avatar component.

--- docs/components/content/AvatarDemo.vue
+++ docs/components/content/AvatarDemo.vue
@@ -3,5 +3,22 @@ import { DestylerAvatar } from '@destyler/avatar/src/avatar'
 </script>
 
 <template>
-  <DestylerAvatar src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+  <DestylerAvatar src="https://elonehoo.me/avatar.png" />
 </template>
+
+<style>
+[destyler="avatar"]{
+  position: relative;
+  display: flex;
+  height: 5rem;
+  width: 5rem;
+  flex-shrink: 0;
+  overflow: hidden;
+  border-radius: 9999px;
+}
+[destyler="avatar-img"]{
+  aspect-ratio: 1/1;
+  height: 100%;
+  width: 100%;
+}
+</style>
