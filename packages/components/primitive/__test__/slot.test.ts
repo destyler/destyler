36684e34e7d949a7692ffd20169fd53b49016d00
elonehoo <hi@elonehoo.me>
2024-01-11 14:23:32 +0800

    test: add slot test

--- packages/components/primitive/__test__/slot.test.ts
+++ packages/components/primitive/__test__/slot.test.ts
@@ -1,10 +1,22 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { defineComponent, h, markRaw } from 'vue'
+import { h } from 'vue'
 import { DestylerSlot } from '../src/slot'
 
-describe('test DestylerPrimitive functionalities', () => {
+describe('test DestylerSlot functionalities', () => {
   it('should work with import on demand', () => {
     mount(DestylerSlot)
+  })
+
+  it('with onClick on itself', async () => {
+    const wrapper = mount(DestylerSlot, {
+      slots: {
+        default: () => h('button', {
+          type: 'button',
+          class: 't',
+        }, 'Click me'),
+      },
+    })
+    expect(wrapper.html()).equal('<button type="button" class="t">Click me</button>')
   })
 })
