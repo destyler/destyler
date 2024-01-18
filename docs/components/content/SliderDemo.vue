b31c0b699aab9c0430ce1794298d465e65d357d0
elonehoo <hi@elonehoo.me>
2024-01-18 10:42:08 +0800

    fix: update css

--- docs/components/content/SliderDemo.vue
+++ docs/components/content/SliderDemo.vue
@@ -13,6 +13,6 @@ const sliderValue = ref([50])
     <DestylerSliderTrack class="relative h-1.5 w-full grow overflow-hidden rounded-full dark:bg-#FAFAFA/20 bg-#18181B/20">
       <DestylerSliderRange class="absolute h-full dark:bg-#FAFAFA bg-#18181B" />
     </DestylerSliderTrack>
-    <DestylerSliderThumb class="block h-4 w-4 rounded-full border dark:bg-#18181B/50 dark:border-#FAFAFA/50 bg-#FFFFFF dark:bg-#09090B shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50" />
+    <DestylerSliderThumb class="block h-4 w-4 rounded-full border dark:border-#18181B/50 dark:border-#FAFAFA/50 bg-#FFFFFF dark:bg-#09090B shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50" />
   </DestylerSliderRoot>
 </template>
