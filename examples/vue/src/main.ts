f6ca12f16298896a21bb4e009071f5a0f1859cdf
elonehoo <hi@elonehoo.me>
2024-01-19 22:13:07 +0800

    perf: a lot of unnecessary modules have been removed to make our debugging easier

--- examples/vue/src/main.ts
+++ examples/vue/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'uno.css'
+import '@unocss/reset/tailwind.css'
 
 createApp(App).mount('#app')
--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import UnoCss from 'unocss/vite'
+import { alias } from '../../vite.path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias,
+  },
   plugins: [
     VueDevTools(),
-    vue(),
+    Vue(),
+    UnoCss(),
   ],
 })
