845bf789a54e0b4d1f41ba4a78754e0d943fe923
elonehoo <hi@elonehoo.me>
2024-02-06 22:58:47 +0800

    feat: add more framework supper

---
+++ docs/components/content/FrameworkList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Box>
+    <FrameworkCard dark-icon="vscode-icons:file-type-vue" light-icon="vscode-icons:file-type-vue" title="Vue" />
+    <FrameworkCard dark-icon="vscode-icons:file-type-nuxt" light-icon="vscode-icons:file-type-nuxt" title="Nuxt" />
+    <FrameworkCard dark-icon="vscode-icons:file-type-astro" light-icon="vscode-icons:file-type-light-astro" title="Astro" />
+    <FrameworkCard dark-icon="vscode-icons:file-type-js" light-icon="vscode-icons:file-type-js" title="Browser" />
+    <FrameworkCard dark-icon="logos:electron" light-icon="logos:electron" title="Electron" />
+    <FrameworkCard dark-icon="simple-icons:nativescript" light-icon="simple-icons:nativescript" title="NativeScript" />
+  </Box>
+</template>
--- docs/content/guide/install.md
+++ docs/content/guide/install.md
@@ -4,8 +4,15 @@ layout: component
 
 # Getting Started
 
+> A quick tutorial to get you up and running with Destyler.
+
 ## Install Destyler
 
 ```bash
 pnpm install destyler
 ```
+
+## Frameworks
+
+:::framework-list
+:::
