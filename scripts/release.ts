9d6fd1aade5e7eb428c6d91ffe28481c92852bbd
elonehoo <hi@elonehoo.me>
2024-02-18 15:51:35 +0800

    chore: add release script

---
+++ scripts/release.ts
@@ -0,0 +1,20 @@
+import { execSync } from 'node:child_process'
+import { fileURLToPath } from 'node:url'
+import path, { dirname } from 'node:path'
+import { consola } from 'consola'
+
+async function run() {
+  consola.info('Start to release the packages...')
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const rootPath = path.join(__dirname, '..')
+  execSync(
+    'npx bumpp package.json packages/*/package.json packages/**/*/package.json --commit --push --tag && pnpm -r publish --access public',
+    {
+      stdio: 'inherit',
+      cwd: rootPath,
+    }
+  )
+}
+
+run()
