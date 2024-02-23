281af212b102f7bd4dc3b42c64307c3fd6a7641b
elonehoo <hi@elonehoo.me>
2024-02-23 17:05:17 +0800

    fix: update dynamic example

--- packages/components/dynamic/demos/Dynamic.demo.vue
+++ packages/components/dynamic/demos/Dynamic.demo.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Icon } from '@iconify/vue'
-import { DestylerDynamicInput, DestylerDynamicItem, DestylerDynamicItemDelete, DestylerDynamicItemText, DestylerDynamicRoot } from '../src'
+import {
+  DestylerDynamicInput,
+  DestylerDynamicItem,
+  DestylerDynamicItemDelete,
+  DestylerDynamicItemText,
+  DestylerDynamicRoot,
+} from '../src'
 
-const modelValue = ref(['Apple', 'Banana'])
+const modelValue = ref(['Vue', 'Nuxt'])
 </script>
 
 <template>
   <DestylerDynamicRoot
     v-model="modelValue"
-    class="flex gap-2 items-center border p-2 rounded-lg w-full max-w-[480px] flex-wrap border-#00000080 bg-white"
+    class="flex gap-2 items-center border p-2 rounded-lg w-full max-w-[480px] flex-wrap border-solid border-b border-black border-op-9 dark:border-white dark:border-op-9 "
   >
-    <DestylerDynamicItem v-for="item in modelValue" :key="item" :value="item" class="text-white flex shadow-md items-center justify-center gap-2 bg-#2F7C57 aria-[current=true]:bg-#30A46C rounded p-1">
+    <DestylerDynamicItem v-for="item in modelValue" :key="item" :value="item" class="dark:text-#09090B text-#FAFAFA flex shadow-md items-center justify-center gap-2 bg-#18181B dark:bg-#FAFAFA rounded p-1">
       <DestylerDynamicItemText class="text-sm pl-1" />
-      <DestylerDynamicItemDelete class="p-0.5 rounded bg-transparent hover:bg-#00000033">
-        <Icon icon="lucide:x" />
+      <DestylerDynamicItemDelete class="p-0.5 rounded bg-transparent hover:bg-#f4f4f5 dark:hover:bg-#27272a hover:bg-op-30! dark:hover:bg-op-30!">
+        x
       </DestylerDynamicItemDelete>
     </DestylerDynamicItem>
 
-    <DestylerDynamicInput placeholder="Fruits..." class="text-sm focus:outline-none flex-1 rounded text-#30A46C bg-transparent placeholder:text-#6F6D78 px-1" />
+    <DestylerDynamicInput placeholder="Fruits..." class="text-sm focus:outline-none flex-1 rounded text-#09090B dark:text-#FAFAFA bg-transparent placeholder:text-#71717A dark:placeholder:text-#A1A1A px-1" />
   </DestylerDynamicRoot>
 </template>
