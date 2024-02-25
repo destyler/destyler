4906cabc3195fd0bb2619f5b1da140e1077e88eb
elonehoo <hi@elonehoo.me>
2024-02-25 21:27:21 +0800

    fix: set dynamic's component export correctly

--- packages/components/dynamic/src/index.ts
+++ packages/components/dynamic/src/index.ts
@@ -2,5 +2,5 @@ export { DestylerDynamicClear, type DestylerDynamicClearProps } from './componen
 export { DestylerDynamicInput, type DestylerDynamicInputProps } from './components/input'
 export { DestylerDynamicItem, type DestylerDynamicItemProops } from './components/item'
 export { DestylerDynamicItemDelete, type DestylerDynamicItemDeleteProps } from './components/itemDelete'
-export { DestylerDynamicItemText, type DestylerDynamicItemTextProps } from './components/itemtext'
+export { DestylerDynamicItemText, type DestylerDynamicItemTextProps } from './components/itemText'
 export { DestylerDynamicRoot, type DestylerDynamicRootProps } from './components/root'
