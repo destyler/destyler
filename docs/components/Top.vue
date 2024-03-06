e0765af0397ca687b3c2a0b6f2129b5037221b42
elonehoo <hi@elonehoo.me>
2024-03-06 21:30:14 +0800

    docs: update back top

--- docs/components/Top.vue
+++ docs/components/Top.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { DestylerBackTop, DestylerIcon } from 'destyler'
+
+const docsBody = ref()
+
+const show = ref<boolean>(false)
+
+onMounted(() => {
+  docsBody.value = document.getElementById('docsBody')
+  show.value = true
+})
 </script>
 
 <template>
-  <div class="">
-    <DestylerBackTop
-      class="fixed bottom-3 flex justify-center items-center w-10 h-10 op30 hover:op100 rounded-full hover-bg-hex-8883 transition duration-300 z-100 print:hidden"
-    >
-      <DestylerIcon name="radix-icons:arrow-up" />
-    </DestylerBackTop>
-  </div>
+  <DestylerBackTop
+    v-if="show"
+    :listen="docsBody"
+    class="fixed bottom-3 flex justify-center items-center w-10 h-10 op30 hover:op100 rounded-full hover-bg-hex-8883 transition duration-300 z-100 print:hidden"
+  >
+    <DestylerIcon name="radix-icons:arrow-up" />
+  </DestylerBackTop>
 </template>
--- docs/layouts/component.vue
+++ docs/layouts/component.vue
@@ -10,7 +10,7 @@ const route = useRoute()
         <div class="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
           <Aside />
           <main class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-            <div class="mx-auto w-full min-w-0 overflow-y-auto h-screen middle-section">
+            <div id="docsBody" class="mx-auto w-full min-w-0 overflow-y-auto h-screen middle-section">
               <Breadcrumb :items="route.path.split('/')" />
               <div class="mb-40">
                 <slot />
