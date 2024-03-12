4efa2827c62f95faa513cb873d2589fcc7b59962
elonehoo <hi@elonehoo.me>
2024-03-12 14:25:16 +0800

    chore: add the type attribute to all packages

--- packages/components/arrow/__test__/basic.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'vitest'
-import { mount } from '@vue/test-utils'
-import { DestylerArrow } from '../src'
-
-describe('basic', () => {
-  it('should work with import on demand', () => {
-    mount(DestylerArrow)
-  })
-})
