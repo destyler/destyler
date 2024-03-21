831ce324ce749f4f8e655f459f52891d6cba1f87
elonehoo <hi@elonehoo.me>
2024-03-21 16:55:34 +0800

    test: add hover card and image

---
+++ packages/components/hoverCard/__test__/hoverCard.test.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { axe } from 'vitest-axe'
+import type { VueWrapper } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import HoverCard from './HoverCard.spec.vue'
+import { sleep } from '~/test/utils'
+
+describe('given a default HoverCard', () => {
+  let wrapper: VueWrapper<InstanceType<typeof HoverCard>>
+  globalThis.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+
+  beforeEach(() => {
+    wrapper = mount(HoverCard, { attachTo: document.body })
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  describe('after mouse enter for a 100ms', () => {
+    it('should pass axe accessibility tests', async () => {
+      await wrapper.find('a').trigger('mouseenter')
+      await sleep(100)
+      expect(await axe(document.body)).toHaveNoViolations()
+    })
+  })
+})
