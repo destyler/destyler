4bfef3f0a0cfa7a959a971c5a3a9ff3eb02319b2
elonehoo <hi@elonehoo.me>
2024-04-30 15:47:13 +0800

    style: run eslint fix

--- packages/components/collapsible/src/components/root.ts
+++ packages/components/collapsible/src/components/root.ts
@@ -4,7 +4,7 @@ import type { AsTag } from '@destyler/primitive'
 import { DestylerPrimitive } from '@destyler/primitive'
 import type { ExtractPublicPropTypes } from '@destyler/shared'
 import { createContext } from '@destyler/shared'
-import { useForwardExpose, useId,useVModel } from '@destyler/composition'
+import { useForwardExpose, useId, useVModel } from '@destyler/composition'
 
 export interface CollapsibleRootContext {
   contentId: string
