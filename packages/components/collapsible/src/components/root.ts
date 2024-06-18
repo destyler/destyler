a0dbdd856dbe8706f6ad1544aa7d312d615990c7
elonehoo <hi@elonehoo.me>
2024-06-18 21:45:54 +0800

    fix: update props

--- packages/components/collapsible/src/components/root.ts
+++ packages/components/collapsible/src/components/root.ts
@@ -1,7 +1,6 @@
-import type { Component, PropType, Ref } from 'vue'
+import type { PropType, Ref } from 'vue'
 import { defineComponent, h } from 'vue'
-import type { AsTag } from '@destyler/primitive'
-import { DestylerPrimitive } from '@destyler/primitive'
+import { DestylerPrimitive, destylerPrimitiveProps } from '@destyler/primitive'
 import type { ExtractPublicPropTypes } from '@destyler/shared'
 import { createContext } from '@destyler/shared'
 import { useForwardExpose, useId, useVModel } from '@destyler/composition'
@@ -17,16 +16,7 @@ export const [injectCollapsibleRootContext, provideCollapsibleRootContext]
   = createContext<CollapsibleRootContext>('DestylerCollapsibleRoot')
 
 export const destylerCollapsibleRootProps = {
-  as: {
-    type: [String, Object] as PropType<AsTag | Component>,
-    required: false,
-    default: 'div',
-  },
-  asChild: {
-    type: Boolean as PropType<boolean>,
-    required: false,
-    default: false,
-  },
+  ...destylerPrimitiveProps,
   defaultOpen: {
     type: Boolean as PropType<boolean>,
     required: false,
