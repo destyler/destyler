1f7dcdeb180dc2c4a6e617ab0e8b2bb3a1f2c289
elonehoo <hi@elonehoo.me>
2024-06-19 15:20:41 +0800

    fix: update breadcrumbs content props

--- packages/components/breadcrumbs/src/components/content.ts
+++ packages/components/breadcrumbs/src/components/content.ts
@@ -1,21 +1,15 @@
-import type { Component, PropType, Ref } from 'vue'
+import type { Ref } from 'vue'
 import { defineComponent, h, onMounted, ref } from 'vue'
-import type { AsTag } from '@destyler/primitive'
-import { DestylerPrimitive } from '@destyler/primitive'
+import { DestylerPrimitive, destylerPrimitiveProps } from '@destyler/primitive'
 import type { ExtractPublicPropTypes } from '@destyler/shared'
 import { createContext } from '@destyler/shared'
 import { useCollection, useForwardExpose } from '@destyler/composition'
 
 export const destylerBreadcrumbsContentProps = {
+  ...destylerPrimitiveProps,
   as: {
-    type: [String, Object] as PropType<AsTag | Component>,
-    required: false,
+    ...destylerPrimitiveProps.as,
     default: 'ul',
-  },
-  asChild: {
-    type: Boolean as PropType<boolean>,
-    required: false,
-    default: false,
   },
 } as const
 
