9a3cd47eade44f900a0dcf72532a839c88786599
elonehoo <hi@elonehoo.me>
2024-07-06 21:44:44 +0800

    wip

--- packages/components/image/src/component.ts
+++ packages/components/image/src/component.ts
@@ -1,3 +1,3 @@
 export { ImageFallback, imageFallbackProps, type ImageFallbackProps } from './components/fallback'
-export { Image, imageProps, type ImageProps } from './components/image'
+export { Image, imageProps, imageEmits, type ImageProps } from './components/image'
 export { ImageRoot, imageRootProps, type ImageRootProps } from './components/root'
