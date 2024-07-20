5a4b53ef39b8d4423dbbf5ee82506d9a429607f8
elonehoo <hi@elonehoo.me>
2024-07-20 15:34:28 +0800

    wip: add icon playground

---
+++ packages/components/icon/.story/Default.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Icon } from '../src'
+</script>
+
+<template>
+  <Story title="Icon/Demo" :layout="{ type: 'single', iframe: true }">
+    <Variant title="default">
+      <div class="flex">
+        <Icon name="logos:vue" class="w-24 h-24 ml-2" />
+        <Icon name="logos:vitejs" class="w-24 h-24 ml-2" />
+      </div>
+    </Variant>
+  </Story>
+</template>
