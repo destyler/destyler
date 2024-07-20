39caba37ed553d7b8595b3027685f5135fae2a46
elonehoo <hi@elonehoo.me>
2024-07-20 15:28:45 +0800

    wip: add hover card playground

---
+++ packages/components/hoverCard/.story/Default.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HoverCard from './components/HoverCard.vue'
+</script>
+
+<template>
+  <Story title="Hover Card/Demo" :layout="{ type: 'single', iframe: true }">
+    <Variant title="default">
+      <HoverCard />
+    </Variant>
+  </Story>
+</template>
