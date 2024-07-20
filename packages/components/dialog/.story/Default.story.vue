4363f814b8b9e7e14ebabdd763165fa25377226c
elonehoo <hi@elonehoo.me>
2024-07-20 13:58:04 +0800

    wip: add dialog playground

---
+++ packages/components/dialog/.story/Default.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Dialog from './components/Dialog.vue'
+</script>
+
+<template>
+  <Story title="Dialog/Default" :layout="{ type: 'single', iframe: false }">
+    <Variant title="default">
+      <Dialog />
+    </Variant>
+  </Story>
+</template>
