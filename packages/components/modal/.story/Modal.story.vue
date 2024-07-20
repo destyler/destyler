7a616f15c2c4fb7822fb5f9b16e4121a3d0d8f6f
elonehoo <hi@elonehoo.me>
2024-07-20 16:28:20 +0800

    wip: add modal playground

---
+++ packages/components/modal/.story/Modal.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Modal from './components/Modal.vue'
+</script>
+
+<template>
+  <Story title="Modal/Demo" :layout="{ type: 'single', iframe: true }">
+    <Variant title="default">
+      <Modal />
+    </Variant>
+  </Story>
+</template>
