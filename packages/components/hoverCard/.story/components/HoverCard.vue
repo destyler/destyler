31695b58e7453e5012d0fcd40252d334e729a66e
elonehoo <hi@elonehoo.me>
2024-07-20 17:20:06 +0800

    wip: add pagination playground

--- packages/components/hoverCard/.story/components/HoverCard.vue
+++ packages/components/hoverCard/.story/components/HoverCard.vue
@@ -14,7 +14,7 @@ const hoverState = ref(false)
 <template>
   <HoverCardRoot v-model:open="hoverState" :open-delay="100">
     <HoverCardTrigger
-      class="inline-block cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
+      class=" bg-white flex justify-center items-center cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
       href="https://github.com/destyler"
       target="_blank"
       rel="noreferrer noopener"
