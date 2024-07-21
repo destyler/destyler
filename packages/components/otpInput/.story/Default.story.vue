6035bfdc5818c583109dd8f1bfbf8d58f641d99f
elonehoo <hi@elonehoo.me>
2024-07-21 17:59:32 +0800

    wip: add otp input playground

---
+++ packages/components/otpInput/.story/Default.story.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { OtpInput, OtpInputRoot } from '..'
+
+const value = ref<string[]>([])
+</script>
+
+<template>
+  <Story title="Otp Input/Default" :layout="{ type: 'single', iframe: false }">
+    <Variant title="default">
+      <div>
+        <label for="otp">Input label</label>
+
+        <OtpInputRoot
+          id="otp"
+          v-model="value"
+          class="flex gap-2 items-center"
+          @complete="e => console.log(e.join(''))"
+        >
+          <OtpInput
+            v-for="(id, index) in 5"
+            :key="id"
+            class="w-10 h-10 rounded text-center text-green10 placeholder:text-mauve5"
+            :index="index"
+          />
+        </OtpInputRoot>
+      </div>
+    </Variant>
+  </Story>
+</template>
