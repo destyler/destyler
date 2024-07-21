e6851231f0d699f084c322de35e96ac4b4741d18
elonehoo <hi@elonehoo.me>
2024-07-21 17:12:34 +0800

    wip: add online playground

---
+++ packages/components/online/.story/Default.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Online } from '../src'
+
+const status = ref<boolean>(false)
+
+function handleStatusChange(value: boolean) {
+  status.value = value
+}
+</script>
+
+<template>
+  <Story title="Online/Default" :layout="{ type: 'single', iframe: false }">
+    <Variant title="default">
+      <Online class="content " @network-status="handleStatusChange">
+        {{ status ? 'Online' : 'Offline' }}
+      </Online>
+    </Variant>
+  </Story>
+</template>
