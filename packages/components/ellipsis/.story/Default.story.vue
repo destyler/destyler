13e8744562d568a364a4174a463b07d04d3aa248
elonehoo <hi@elonehoo.me>
2024-07-21 16:47:55 +0800

    wip: add ellipsis playground

---
+++ packages/components/ellipsis/.story/Default.story.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { EllipsisContent, EllipsisPortal, EllipsisProvider, EllipsisRoot, EllipsisTrigger } from '../src'
+</script>
+
+<template>
+  <Story title="Ellipsis/Default" :layout="{ type: 'single', iframe: false }">
+    <Variant title="default">
+      <EllipsisProvider>
+        <EllipsisRoot>
+          <EllipsisTrigger class="max-w-240px">
+            住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪
+          </EllipsisTrigger>
+          <EllipsisPortal>
+            <EllipsisContent
+              :side-offset="5"
+            />
+          </EllipsisPortal>
+        </EllipsisRoot>
+      </EllipsisProvider>
+    </Variant>
+  </Story>
+</template>
