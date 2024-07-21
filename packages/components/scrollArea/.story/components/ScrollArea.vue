1ba56592866e2b034bc9422af3653664489cfdd1
elonehoo <hi@elonehoo.me>
2024-07-21 10:25:26 +0800

    wip: add scroll area playground

---
+++ packages/components/scrollArea/.story/components/ScrollArea.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  ScrollAreaCorner,
+  ScrollAreaRoot,
+  ScrollAreaScrollbar,
+  ScrollAreaThumb,
+  ScrollAreaViewport,
+} from '../../src'
+import ScrollAreaCopy from './ScrollAreaCopy.vue'
+
+interface Props {
+  type?: any | undefined
+  dir?: any | undefined
+  scrollHideDelay?: number | undefined
+  as?: any | undefined
+  asChild?: boolean | undefined
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <ScrollAreaRoot
+    style="width: 200px; height: 200px; overflow: hidden;"
+    v-bind="props"
+  >
+    <ScrollAreaViewport style="width: 100%; height: 100%;">
+      <ScrollAreaCopy style="width: 50px;" />
+      <ScrollAreaCopy style="width: 50px;" />
+    </ScrollAreaViewport>
+    <ScrollAreaScrollbar
+      orientation="vertical"
+    >
+      <ScrollAreaThumb />
+    </ScrollAreaScrollbar>
+
+    <ScrollAreaCorner />
+  </ScrollAreaRoot>
+</template>
