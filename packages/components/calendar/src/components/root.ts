e811a5641153f9770c8eab441feffef62a59cbb8
elonehoo <hi@elonehoo.me>
2024-07-21 18:45:48 +0800

    wip

--- packages/components/calendar/src/components/root.ts
+++ packages/components/calendar/src/components/root.ts
@@ -1,12 +1,12 @@
 import type { PropType, Ref, SlotsType, VNode } from 'vue'
-import { computed, defineComponent, h, onMounted, toRefs, watch } from 'vue'
+import { defineComponent, h, onMounted, toRefs, watch } from 'vue'
 import { Primitive, primitiveProps } from '@destyler/primitive'
 import type { Direction, ExtractPublicPropTypes, Grid, Matcher, SupportedLocale, WeekDayFormat } from '@destyler/shared'
-import { createContext, createDecade, createYear, getDefaultDate, handleCalendarInitialFocus } from '@destyler/shared'
+import { createContext, getDefaultDate, handleCalendarInitialFocus } from '@destyler/shared'
 import { useDirection, useForwardExpose, useVModel } from '@destyler/composition'
 import type { Formatter } from '@destyler/composition'
 import { isEqualDay, isSameDay } from '@internationalized/date'
-import type { CalendarDate, CalendarDateTime, DateValue, ZonedDateTime } from '@internationalized/date'
+import type { DateValue } from '@internationalized/date'
 
 import { useCalendar, useCalendarState } from '../composition/use-calendar'
 
