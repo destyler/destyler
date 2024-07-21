c8530d40c242aa80d5b757af9f24f454c24e4f16
elonehoo <hi@elonehoo.me>
2024-07-21 17:24:46 +0800

    wip: add qr code playground

---
+++ packages/components/qrCode/.story/Default.story.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {
+  QrCodeItem,
+  QrCodeRoot,
+} from '../src'
+</script>
+
+<template>
+  <Story title="QR Code/Default" :layout="{ type: 'single', iframe: false }">
+    <Variant title="default">
+      <QrCodeRoot value="https://elonehoo.me">
+        <QrCodeItem />
+      </QrCodeRoot>
+    </Variant>
+  </Story>
+</template>
