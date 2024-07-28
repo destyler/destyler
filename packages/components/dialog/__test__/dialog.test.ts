12d2f8398145840daebe5e3b47a5edad6a0b5df4
elonehoo <hi@elonehoo.me>
2024-07-28 17:48:41 +0800

    test: add dialog spec

---
+++ packages/components/dialog/__test__/dialog.test.ts
@@ -0,0 +1,117 @@
+import { type Mock, afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { axe } from 'vitest-axe'
+import type { DOMWrapper, VueWrapper } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import { defineComponent, nextTick } from 'vue'
+import { findByText, fireEvent, render } from '@testing-library/vue'
+import { DialogClose, DialogContent, DialogOverlay, DialogRoot, DialogTitle, DialogTrigger } from '../src'
+
+const OPEN_TEXT = 'Open'
+const CLOSE_TEXT = 'Close'
+const TITLE_TEXT = 'Title'
+
+const NoLabelDialogTest = defineComponent({
+  components: { DialogRoot, DialogTrigger, DialogOverlay, DialogContent, DialogClose },
+  template: `<DialogRoot>
+  <DialogTrigger>${OPEN_TEXT}</DialogTrigger>
+  <DialogOverlay />
+  <DialogContent>
+    <DialogClose>${CLOSE_TEXT}</DialogClose>
+  </DialogContent>
+</DialogRoot>`,
+})
+
+const UndefinedDescribedByDialog = defineComponent({
+  components: { DialogRoot, DialogTrigger, DialogOverlay, DialogContent, DialogClose, DialogTitle },
+  template: `  <DialogRoot>
+  <DialogTrigger>${OPEN_TEXT}</DialogTrigger>
+  <DialogOverlay />
+  <DialogContent :aria-describedby="undefined">
+    <DialogTitle>${TITLE_TEXT}</DialogTitle>
+    <DialogClose>${CLOSE_TEXT}</DialogClose>
+  </DialogContent>
+</DialogRoot>`,
+})
+
+function renderAndClickDialogTrigger(Dialog: any) {
+  fireEvent.click(render(Dialog).getByText(OPEN_TEXT))
+}
+
+const DialogTest = defineComponent({
+  components: { DialogRoot, DialogTrigger, DialogOverlay, DialogContent, DialogClose, DialogTitle },
+  template: `<DialogRoot >
+  <DialogTrigger>${OPEN_TEXT}</DialogTrigger>
+  <DialogOverlay />
+  <DialogContent>
+    <DialogTitle>${TITLE_TEXT}</DialogTitle>
+    <DialogClose>${CLOSE_TEXT}</DialogClose>
+  </DialogContent>
+</DialogRoot>`,
+})
+
+describe('given a default Dialog', () => {
+  let wrapper: VueWrapper<InstanceType<typeof DialogTest>>
+  let trigger: DOMWrapper<HTMLElement>
+  let closeButton: HTMLElement
+  let consoleWarnMock: any
+  let consoleWarnMockFunction: Mock
+
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    wrapper = mount(DialogTest, { attachTo: document.body })
+    trigger = wrapper.find('button')
+    consoleWarnMockFunction = vi.fn()
+    consoleWarnMock = vi.spyOn(console, 'warn').mockImplementation(consoleWarnMockFunction)
+  })
+
+  afterEach(() => {
+    consoleWarnMock.mockRestore()
+    consoleWarnMockFunction.mockClear
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(document.body)).toHaveNoViolations()
+
+    // open modal
+    wrapper.find('button').element.click()
+    await nextTick()
+    expect(await axe(document.body)).toHaveNoViolations()
+  })
+
+  describe('after clicking the trigger', () => {
+    beforeEach(async () => {
+      fireEvent.click(trigger.element)
+      closeButton = await findByText(document.body, CLOSE_TEXT)
+    })
+
+    describe('when aria-describedby is set to undefined', () => {
+      beforeEach(() => {
+        document.body.innerHTML = ''
+      })
+      it('should not warn to the console', () => {
+        consoleWarnMockFunction.mockClear()
+
+        renderAndClickDialogTrigger(UndefinedDescribedByDialog)
+        expect(consoleWarnMockFunction).not.toHaveBeenCalled()
+      })
+    })
+
+    it('should open the content', () => {
+      expect(document.body.innerHTML).toContain(closeButton.innerHTML)
+    })
+
+    it('should focus the close button', () => {
+      expect(closeButton).toBe(document.activeElement)
+    })
+
+    describe('when pressing escape', () => {
+      beforeEach(() => {
+        fireEvent.keyDown(document.activeElement!, { key: 'Escape' })
+      })
+
+      it('should close the content', () => {
+        expect(document.body.innerHTML).not.toContain(closeButton.innerHTML)
+      })
+    })
+  })
+})
