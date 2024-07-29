75920904ca572d46145d18f2697a067fca3ead94
elonehoo <hi@elonehoo.me>
2024-07-29 19:01:50 +0800

    wip

---
+++ packages/components/switch/__test__/Switch.spec.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { SwitchRoot, SwitchThumb } from '../src'
+
+const checked = ref(false)
+</script>
+
+<template>
+  <p>{{ checked ? "checked" : "unchecked" }}</p>
+  <div>
+    <label for="airplane-mode">Airplane mode</label>
+    <SwitchRoot id="airplane-mode" v-model:checked="checked" aria-label="Airplane mode" data-testId="root" name="test" value="true">
+      <SwitchThumb data-testId="thumb" />
+    </SwitchRoot>
+  </div>
+</template>
