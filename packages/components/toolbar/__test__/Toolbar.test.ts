83f1e6b5e1e80b1864b0e2863dd34915cc132fc0
elonehoo <hi@elonehoo.me>
2024-07-30 09:41:44 +0800

    wip

---
+++ packages/components/toolbar/__test__/Toolbar.test.ts
@@ -0,0 +1,24 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { DOMWrapper, VueWrapper } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import { axe } from 'vitest-axe'
+import Toolbar from './Toolbar.spec.vue'
+
+describe('default Toolbar', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Toolbar>>
+  let triggers: DOMWrapper<HTMLElement>[]
+
+  beforeEach(() => {
+    wrapper = mount(Toolbar, { attachTo: document.body })
+    triggers = wrapper.findAll('button')
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('should have default selected value', () => {
+    const selected = triggers.filter(i => i.attributes('data-state') === 'on').map(i => i.element)
+    expect(selected.includes(triggers[4].element)).toBeTruthy()
+  })
+})
