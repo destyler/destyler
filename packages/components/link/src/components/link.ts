360ef42f2c1e5fc713bdd36adaf4b737f658cf28
elonehoo <hi@elonehoo.me>
2024-07-30 11:50:56 +0800

    wip

--- packages/components/link/src/components/link.ts
+++ packages/components/link/src/components/link.ts
@@ -2,6 +2,7 @@ import type { PropType, SlotsType, VNode } from 'vue'
 import { computed, defineComponent, h, mergeProps } from 'vue'
 import { Primitive, primitiveProps } from '@destyler/primitive'
 import type { ExtractPublicPropTypes } from '@destyler/shared'
+import { useForwardExpose } from '@destyler/composition'
 
 export const linkProps = {
   asChild: {
@@ -46,22 +47,30 @@ export const Link = defineComponent({
     default: () => VNode[]
   }>,
   setup(props) {
+    const { forwardRef, currentElement } = useForwardExpose()
     const hasTarget = computed(() => props.target && props.target !== '_self')
+
+    const isTagName = computed(() => {
+      return currentElement.value?.tagName.toLowerCase()
+    })
 
     return {
       hasTarget,
+      isTagName,
+      forwardRef,
     }
   },
   render() {
     return h(Primitive, mergeProps(this.$attrs, {
+      'ref': this.forwardRef,
       'as': 'a',
       'asChild': this.$props.asChild,
-      'role': 'link',
+      'role': this.isTagName !== 'a' || this.$props.disabled ? 'link' : undefined,
       'href': this.$props.disabled ? undefined : this.$props.to,
       'target': this.hasTarget ? this.$props.target : undefined,
       'rel': this.$props.noRel ? undefined : this.$props.rel,
       'noRel': this.$props.noRel ? undefined : this.$props.noRel,
-      'tabindex': this.$props.disabled ? undefined : 0,
+      'tabindex': this.$props.disabled ? 0 : undefined,
       'aria-disabled': this.$props.disabled ? true : undefined,
       'data-disabled': this.$props.disabled ? '' : undefined,
     }), () => this.$slots.default?.())
