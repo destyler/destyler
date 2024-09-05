9f1e9bbc8ec5ff3593ca0d990137795c7341cd65
elonehoo <hi@elonehoo.me>
2024-09-05 22:34:53 +0800

    wip

--- .docs/snippets/popover/index.vue
+++ .docs/snippets/popover/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { Icon } from '@destyler/icon'
 import {
-
+  PopoverRoot,
+  PopoverTrigger,
 } from '@destyler/popover'
 </script>
 
 <template>
+  <PopoverRoot>
+    <PopoverTrigger>Open popover</PopoverTrigger>
+  </PopoverRoot>
 </template>
