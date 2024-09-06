3e1a73c8f59004d57d6f61d777a47a61fc5a8c38
elonehoo <hi@elonehoo.me>
2024-09-06 11:14:14 +0800

    wip: add popover docs module

--- .docs/snippets/popover/css/index.vue
+++ .docs/snippets/popover/css/index.vue
@@ -1,62 +1,83 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { Icon } from '@destyler/icon'
+import { Label } from '@destyler/label'
 import {
-  PaginationEllipsis,
-  PaginationFirst,
-  PaginationLast,
-  PaginationList,
-  PaginationListItem,
-  PaginationNext,
-  PaginationPrev,
-  PaginationRoot,
-} from '@destyler/pagination'
+  PopoverClose,
+  PopoverContent,
+  PopoverPortal,
+  PopoverRoot,
+  PopoverTrigger,
+} from '@destyler/popover'
 import './style.css'
+
+const forms = ref([
+  {
+    label: 'Width',
+    value: '100%',
+  },
+  {
+    label: 'Max. width',
+    value: '100px',
+  },
+  {
+    label: 'Height',
+    value: '25px',
+  },
+  {
+    label: 'Max. height',
+    value: 'none',
+  },
+])
 </script>
 
 <template>
-  <PaginationRoot
-    :total="100"
-    :sibling-count="1"
-    show-edges
-    :default-page="2"
-  >
-    <PaginationList
-      v-slot="{ items }"
-      class="pagination-list"
+  <PopoverRoot>
+    <PopoverTrigger
+      class="popover-trigger"
     >
-      <PaginationFirst
-        class="item"
+      Open popover
+    </PopoverTrigger>
+    <PopoverPortal>
+      <PopoverContent
+        side="bottom"
+        :side-offset="5"
+        class="popover-content"
       >
-        <Icon name="radix-icons:double-arrow-left" />
-      </PaginationFirst>
-      <PaginationPrev
-        class="item"
-      >
-        <Icon name="radix-icons:chevron-left" />
-      </PaginationPrev>
-      <template v-for="(page, index) in items">
-        <PaginationListItem
-          v-if="page.type === 'page'"
-          :key="index"
-          :value="page.value"
-          class="item"
-        />
-        <PaginationEllipsis
-          v-else
-          :key="page.type"
-          :index="index"
-        />
-      </template>
-      <PaginationNext
-        class="item"
-      >
-        <Icon name="radix-icons:chevron-right" />
-      </PaginationNext>
-      <PaginationLast
-        class="item"
-      >
-        <Icon name="radix-icons:double-arrow-right" />
-      </PaginationLast>
-    </PaginationList>
-  </PaginationRoot>
+        <div class="box">
+          <div class="space-y-2">
+            <h4 class="title">
+              Dimensions
+            </h4>
+            <p class="desc">
+              Set the dimensions for the layer.
+            </p>
+          </div>
+          <div class="item-box">
+            <div
+              v-for="form in forms"
+              :key="form.label"
+              class="item"
+            >
+              <Label
+                for="width"
+                class="label"
+              >
+                {{ form.label }}
+              </Label>
+              <input
+                v-model="form.value"
+                class="input"
+              >
+            </div>
+          </div>
+        </div>
+        <PopoverClose
+          class="popover-close"
+        >
+          <Icon name="carbon:close-large" />
+        </PopoverClose>
+      </PopoverContent>
+    </PopoverPortal>
+  </PopoverRoot>
 </template>
