b4ee3d6831e82c1953713f6d3347a0d4c29dee04
elonehoo <hi@elonehoo.me>
2024-09-21 09:38:45 +0800

    ci

--- scripts/docs/zip.ts
+++ scripts/docs/zip.ts
@@ -8,7 +8,6 @@ function run() {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = dirname(__filename)
   const outPath = join(__dirname, '../..', '.docs', '.vitepress', 'zip', 'dist.zip')
-  console.log('outPath', outPath)
   const inPath = join(__dirname, '../..', '.docs', '.vitepress', 'dist')
 
   fs.mkdir(join(__dirname, '../..', '.docs', '.vitepress', 'zip'), { recursive: true })
