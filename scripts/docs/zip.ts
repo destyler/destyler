a77e450abca6b42647143c3c1cb8e81222f7ef62
Elone Hoo <hi@elonehoo.me>
2024-09-23 10:08:22 +0800

    ci: add docs build deploy (#72)
    when a new version is released, some modules will be automatically deployed, and you can also manually deploy them in advance.

---
+++ scripts/docs/zip.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import fs from 'fs-extra'
+import { handleZIP } from '../zip'
+
+function run() {
+  //
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const outPath = join(__dirname, '../..', '.docs', '.vitepress', 'zip', 'dist.zip')
+  const inPath = join(__dirname, '../..', '.docs', '.vitepress', 'dist')
+
+  fs.mkdir(join(__dirname, '../..', '.docs', '.vitepress', 'zip'), { recursive: true })
+
+  handleZIP(inPath, outPath)
+}
+
+run()
