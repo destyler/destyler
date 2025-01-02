4358bdea2e4f781f98b65630d4335de0c0cfbd8d
elonehoo <hi@elonehoo.me>
2025-01-02 20:07:42 +0800

    wip

--- examples/vue/src/pages/dialog.vue
+++ examples/vue/src/pages/dialog.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import * as dialog from "@destyler/dialog";
 import { normalizeProps, useMachine } from "@destyler/vue";
-import { computed, Teleport, useId } from "vue";
+import { computed, useId } from "vue";
 
 const [state, send] = useMachine(dialog.machine({
   id: useId(),
@@ -15,20 +15,18 @@ const api = computed(() => dialog.connect(state.value, send, normalizeProps));
 
 <template>
   <button ref="ref" v-bind="api.getTriggerProps()">Open Dialog</button>
-  <Teleport to="body">
-    <div>
-      <div v-bind="api.getBackdropProps()" />
-      <div v-bind="api.getPositionerProps()">
-        <div v-bind="api.getContentProps()">
-          <h2 v-bind="api.getTitleProps()">Edit profile</h2>
-          <p v-bind="api.getDescriptionProps()">
-            Make changes to your profile here. Click save when you are done.
-          </p>
-          <button v-bind="api.getCloseTriggerProps()">X</button>
-          <input placeholder="Enter name..." />
-          <button>Save Changes</button>
-        </div>
+  <div>
+    <div v-bind="api.getBackdropProps()" />
+    <div v-bind="api.getPositionerProps()">
+      <div v-bind="api.getContentProps()">
+        <h2 v-bind="api.getTitleProps()">Edit profile</h2>
+        <p v-bind="api.getDescriptionProps()">
+          Make changes to your profile here. Click save when you are done.
+        </p>
+        <button v-bind="api.getCloseTriggerProps()">X</button>
+        <input placeholder="Enter name..." />
+        <button>Save Changes</button>
       </div>
     </div>
-  </Teleport>
+  </div>
 </template>
