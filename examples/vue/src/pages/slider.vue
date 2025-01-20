7e69dac9e0be19db79f7e13a6c6735cbd2306606
elonehoo <hi@elonehoo.me>
2025-01-20 21:20:09 +0800

    wip: add slider

---
+++ examples/vue/src/pages/slider.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import * as slider from "@destyler/slider";
+import { normalizeProps, useMachine } from "@destyler/vue";
+import { computed,useId } from "vue";
+
+const [state, send] = useMachine(slider.machine({ id: useId(), value: [0] }));
+const api = computed(() => slider.connect(state.value, send, normalizeProps));
+</script>
+
+<template>
+  <div ref="ref" v-bind="api.getRootProps()">
+    <div>
+      <label v-bind="api.getLabelProps()">Slider Label</label>
+      <output v-bind="api.getValueTextProps()">{{ api.value.at(0) }}</output>
+    </div>
+    <div v-bind="api.getControlProps()" class="">
+      <div v-bind="api.getTrackProps()" class="w-full h-10px bg-green">
+        <div v-bind="api.getRangeProps()" class="h-10px bg-yellow" />
+      </div>
+      <div
+        class="w-10px h-10px bg-red"
+        v-for="(_, index) in api.value"
+        :key="index"
+        v-bind="api.getThumbProps({ index })"
+      >
+        <input v-bind="api.getHiddenInputProps({ index })" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ packages/components/slider/src/anatomy.ts
@@ -0,0 +1,16 @@
+import { createAnatomy } from '@zag-js/anatomy'
+
+export const anatomy = createAnatomy('slider').parts(
+  'root',
+  'label',
+  'thumb',
+  'valueText',
+  'track',
+  'range',
+  'control',
+  'markerGroup',
+  'marker',
+  'draggingIndicator',
+)
+
+export const parts = anatomy.build()
