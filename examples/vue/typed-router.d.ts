a39f120b6ab427d7ddbc50b66a7ce9f38d4a8b33
elonehoo <hi@elonehoo.me>
2025-01-21 19:40:22 +0800

    wip: add progress

--- examples/vue/typed-router.d.ts
+++ examples/vue/typed-router.d.ts
@@ -28,6 +28,7 @@ declare module 'vue-router/auto-routes' {
     '/menu': RouteRecordInfo<'/menu', '/menu', Record<never, never>, Record<never, never>>,
     '/pagination': RouteRecordInfo<'/pagination', '/pagination', Record<never, never>, Record<never, never>>,
     '/popover': RouteRecordInfo<'/popover', '/popover', Record<never, never>, Record<never, never>>,
+    '/progress': RouteRecordInfo<'/progress', '/progress', Record<never, never>, Record<never, never>>,
     '/slider': RouteRecordInfo<'/slider', '/slider', Record<never, never>, Record<never, never>>,
     '/splitter': RouteRecordInfo<'/splitter', '/splitter', Record<never, never>, Record<never, never>>,
     '/switch': RouteRecordInfo<'/switch', '/switch', Record<never, never>, Record<never, never>>,
