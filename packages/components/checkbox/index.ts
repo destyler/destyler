8b99138c1fd51dab79c064d0ac1ac8b2b5d9f50f
elonehoo <hi@elonehoo.me>
2025-02-05 21:16:03 +0800

    docs: add packages description

--- packages/components/checkbox/index.ts
+++ packages/components/checkbox/index.ts
@@ -3,10 +3,10 @@ export { connect } from './src/connect'
 export { machine } from './src/machine'
 export * from './src/props'
 export type {
-  MachineApi as Api,
   CheckedChangeDetails,
   CheckedState,
-  UserDefinedContext as Context,
   ElementIds,
+  MachineApi as Api,
   Service,
+  UserDefinedContext as Context,
 } from './src/types'
