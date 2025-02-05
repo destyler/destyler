325fcb44bf6cb7a743d88aae0648f1a9b28af190
elonehoo <hi@elonehoo.me>
2025-02-05 22:18:47 +0800

    chore: add layout and home card

--- examples/vue/uno.config.ts
+++ examples/vue/uno.config.ts
@@ -1,5 +1,17 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+} from 'unocss'
 
 export default defineConfig({
-
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+  ],
 })
