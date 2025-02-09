e8747e5e2724b1ff36cb6efcd3798b998d8470a4
elonehoo <hi@elonehoo.me>
2025-02-09 14:46:32 +0800

    wip

--- shared/src/controls.ts
+++ shared/src/controls.ts
@@ -120,26 +120,34 @@ export const menuControls = defineControls({
 })
 
 export const numberInputControls = defineControls({
-  disabled: { type: "boolean", defaultValue: false },
-  clampValueOnBlur: { type: "boolean", defaultValue: true },
-  allowMouseWheel: { type: "boolean", defaultValue: false },
-  spinOnPress: { type: "boolean", defaultValue: true },
-  step: { type: "number", defaultValue: 1 },
-  min: { type: "number", defaultValue: 0 },
-  max: { type: "number", defaultValue: 100 },
-  locale: {
-    type: "select",
-    options: ["en-US", "en-GB", "fr-FR", "de-DE", "ja-JP", "mk-MK", "zh-CN"] as const,
+  "disabled": { type: 'boolean', defaultValue: false },
+  "clampValueOnBlur": { type: 'boolean', defaultValue: true },
+  "allowMouseWheel": { type: 'boolean', defaultValue: false },
+  "spinOnPress": { type: 'boolean', defaultValue: true },
+  "step": { type: 'number', defaultValue: 1 },
+  "min": { type: 'number', defaultValue: 0 },
+  "max": { type: 'number', defaultValue: 100 },
+  "locale": {
+    type: 'select',
+    options: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'mk-MK', 'zh-CN'] as const,
   },
-  "formatOptions.maximumFractionDigits": { type: "number" },
-  "formatOptions.minimumFractionDigits": { type: "number" },
-  "formatOptions.style": {
-    type: "select",
-    options: ["decimal", "currency", "percent"] as const,
+  'formatOptions.maximumFractionDigits': { type: 'number' },
+  'formatOptions.minimumFractionDigits': { type: 'number' },
+  'formatOptions.style': {
+    type: 'select',
+    options: ['decimal', 'currency', 'percent'] as const,
   },
-  "formatOptions.currency": {
-    type: "select",
-    defaultValue: "USD",
-    options: ["USD", "EUR", "JPY", "GBP", "MXN", "CNY"] as const,
+  'formatOptions.currency': {
+    type: 'select',
+    defaultValue: 'USD',
+    options: ['USD', 'EUR', 'JPY', 'GBP', 'MXN', 'CNY'] as const,
   },
 })
+
+export const pinInputControls = defineControls({
+  mask: { type: 'boolean', defaultValue: false },
+  otp: { type: 'boolean', defaultValue: false },
+  blurOnComplete: { type: 'boolean', defaultValue: false },
+  type: { type: 'select', options: ['numeric', 'alphanumeric', 'alphabetic'] as const, defaultValue: 'numeric' },
+})
+
