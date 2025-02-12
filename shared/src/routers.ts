309974828ddd61bba82e69a605024802daf626c0
elonehoo <hi@elonehoo.me>
2025-02-12 14:38:07 +0800

    wip

--- shared/src/routers.ts
+++ shared/src/routers.ts
@@ -44,4 +44,36 @@ export const routes: RouteData[] = [
     path: '/color-picker',
     label: 'Color Picker',
   },
+  {
+    path: '/combobox',
+    label: 'Combobox',
+  },
+  {
+    path: '/dialog',
+    label: 'Dialog',
+  },
+  {
+    path: '/divider',
+    label: 'Divider',
+  },
+  {
+    path: '/dynamic',
+    label: 'Dynamic',
+  },
+  {
+    path: '/edit',
+    label: 'Edit',
+  },
+  {
+    path: '/file-upload',
+    label: 'File Upload',
+  },
+  {
+    path: '/floating-panel',
+    label: 'Floating Panel',
+  },
+  {
+    path: '/hover-card',
+    label: 'Hover Card',
+  },
 ]
