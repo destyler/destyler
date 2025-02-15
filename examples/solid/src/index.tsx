6c809b24b2e83bb99221a3144869dfc1a40b3495
elonehoo <hi@elonehoo.me>
2025-02-15 16:38:36 +0800

    wip

--- examples/solid/src/index.tsx
+++ examples/solid/src/index.tsx
@@ -1,17 +1,25 @@
 import { Router } from '@solidjs/router'
 import { render } from 'solid-js/web'
 import routes from '~solid-pages'
-import '@unocss/reset/tailwind.css'
-import 'uno.css'
 
+import DefaultLayout from './layouts/default'
+import '@unocss/reset/tailwind.css'
+
+import 'uno.css'
 import '../../../shared/src/bootstrap.css'
 
 const root = document.getElementById('root')
 
-render(() => {
+function App() {
   return (
-    <Router>
-      {routes}
-    </Router>
+    <DefaultLayout>
+      <>
+        <Router>
+          {routes}
+        </Router>
+      </>
+    </DefaultLayout>
   )
-}, root!)
+}
+
+render(() => <App />, root!)
--- examples/solid/src/pages/index.tsx
+++ examples/solid/src/pages/index.tsx
@@ -1,30 +1,29 @@
-import { createSignal } from 'solid-js'
+import { routes } from '@destyler/shared'
+import { useNavigate } from '@solidjs/router'
 
-function App() {
-  const [count, setCount] = createSignal(0)
+function Home() {
+  const navigate = useNavigate()
 
   return (
-    <>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is
-          {' '}
-          {count()}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
+    <div class="p-3">
+      <h1 class="font-bold text-2xl mb-4">
+        Destyler + Solid
+      </h1>
+
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-3">
+        {routes.map(route => (
+          <div
+            class="p-2 border rounded-lg hover:shadow-md cursor-pointer transition-shadow text-center"
+            onClick={() => navigate(route.path)}
+          >
+            <h2 class="text-sm font-medium capitalize truncate">
+              {route.label}
+            </h2>
+          </div>
+        ))}
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default Home
