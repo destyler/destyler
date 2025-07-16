47e7418933ff61f9d9e7af6e34866442b764aaae
elonehoo <hi@elonehoo.me>
2025-07-16 20:17:21 +0800

    wip

--- packages/components/toast/test/toast.vue.browser.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'vitest'
-// import Toast from '~/vue/'
-
-describe.skip('toast vue browser tests', () => {
-  it('renders correctly', async () => {
-    // render(Toast)
-    // await Tests.RendersCorrectly()
-  })
-})
