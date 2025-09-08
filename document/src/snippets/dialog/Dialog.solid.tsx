f872f03d28fe0dbd22ba3d940d86be0b176a3d01
elonehoo <hi@elonehoo.me>
2025-09-08 18:39:56 +0800

    wip: content menu

--- document/src/snippets/dialog/Dialog.solid.tsx
+++ document/src/snippets/dialog/Dialog.solid.tsx
@@ -3,6 +3,7 @@ import * as dialog from '@destyler/dialog'
 import { normalizeProps, useMachine } from '@destyler/solid'
 import { createMemo, createUniqueId } from 'solid-js'
 import { Portal } from 'solid-js/web'
+import '../../styles/components/dialog.css'
 
 export default function Dialog() {
   const [state, send] = useMachine(dialog.machine({
@@ -13,51 +14,27 @@ export default function Dialog() {
 
   return (
     <>
-      <button
-        {...api().getTriggerProps()}
-        class="btn"
-      >
+      <button {...api().getTriggerProps()}>
         Open Dialog
       </button>
 
       { api().open && (
         <Portal mount={document.body}>
-          <div>
-            <div
-              {...api().getBackdropProps()}
-              class="fixed inset-0 z-100 bg-background/80 backdrop-blur-sm"
-            />
-            <div
-              {...api().getPositionerProps()}
-              class="fixed z-101 inset-0 flex items-center justify-center"
-            >
-              <div
-                {...api().getContentProps()}
-                class="bg-background border border-border! shadow-lg rounded-lg w-full max-w-md relative p-6"
-              >
-                <h2
-                  {...api().getTitleProps()}
-                  class="text-lg font-semibold text-foreground mb-4"
-                >
+          <div data-layout="sinppets">
+            <div {...api().getBackdropProps()} />
+            <div {...api().getPositionerProps()}>
+              <div {...api().getContentProps()}>
+                <h2 {...api().getTitleProps()}>
                   Edit profile
                 </h2>
-                <p
-                  {...api().getDescriptionProps()}
-                  class="text-muted-foreground mb-6"
-                >
+                <p {...api().getDescriptionProps()}>
                   Make changes to your profile here. Click save when you are done.
                 </p>
-                <button
-                  {...api().getCloseTriggerProps()}
-                  class="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100"
-                >
-                  <div class="w-4 h-4 i-carbon:close-large" />
+                <button {...api().getCloseTriggerProps()}>
+                  <div />
                 </button>
-                <input
-                  placeholder="Enter name..."
-                  class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                />
-                <button class="btn mt-4 w-full justify-center">
+                <input placeholder="Enter name..." />
+                <button>
                   Save Changes
                 </button>
               </div>
--- document/src/snippets/dialog/Dialog.svelte
+++ document/src/snippets/dialog/Dialog.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import * as dialog from '@destyler/dialog'
   import { normalizeProps, useMachine, portal } from '@destyler/svelte'
+  import '../../styles/components/dialog.css'
+
   const id = $props.id()
 
   const [state, send] = useMachine(dialog.machine({ id }))
@@ -8,48 +10,26 @@
   const api = $derived(dialog.connect(state, send, normalizeProps))
 </script>
 
-<button {...api.getTriggerProps()} class="btn">
+<button {...api.getTriggerProps()}>
   Open Dialog
 </button>
 
 {#if api.open}
-  <div use:portal>
-    <div
-      {...api.getBackdropProps()}
-      class="fixed inset-0 z-100 bg-background/80 backdrop-blur-sm"
-    ></div>
-    <div
-      {...api.getPositionerProps()}
-      class="fixed z-101 inset-0 flex items-center justify-center"
-    >
-      <div
-        {...api.getContentProps()}
-        class="bg-background border border-border! shadow-lg rounded-lg w-full max-w-md relative p-6"
-      >
-        <h2
-          {...api.getTitleProps()}
-          class="text-lg font-semibold text-foreground mb-4"
-        >
+  <div data-layout="sinppets" use:portal>
+    <div {...api.getBackdropProps()} ></div>
+    <div {...api.getPositionerProps()}>
+      <div {...api.getContentProps()}>
+        <h2 {...api.getTitleProps()}>
           Edit profile
         </h2>
-        <p
-          {...api.getDescriptionProps()}
-          class="text-muted-foreground mb-6"
-        >
+        <p {...api.getDescriptionProps()}>
           Make changes to your profile here. Click save when you are done.
         </p>
-        <button
-          {...api.getCloseTriggerProps()}
-          class="absolute right-4 top-4 rounded-sm opacity-70
-          transition-opacity hover:opacity-100"
-        >
-          <div class="w-4 h-4 i-carbon:close-large" ></div>
+        <button {...api.getCloseTriggerProps()}>
+          <div ></div>
         </button>
-        <input
-          placeholder="Enter name..."
-          class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-        >
-        <button class="btn mt-4 w-full justify-center">
+        <input placeholder="Enter name...">
+        <button >
           Save Changes
         </button>
       </div>
