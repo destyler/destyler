8b77d76f8f3cfd368d57b509debb7e168fa70f9b
elonehoo <hi@elonehoo.me>
2025-09-28 14:24:20 +0800

    wip: switch

--- document/src/snippets/switch/Switch.solid.tsx
+++ document/src/snippets/switch/Switch.solid.tsx
@@ -2,6 +2,7 @@
 import { normalizeProps, useMachine } from '@destyler/solid'
 import * as switchs from '@destyler/switch'
 import { createMemo, createUniqueId } from 'solid-js'
+import '../../styles/components/switch.css'
 
 export default function Switch() {
   const [state, send] = useMachine(switchs.machine({
@@ -12,19 +13,10 @@ export default function Switch() {
   const api = createMemo(() => switchs.connect(state, send, normalizeProps))
 
   return (
-    <label
-      {...api().getRootProps()}
-      class="flex items-center mt-0! justify-center cursor-pointer disabled:cursor-not-allowed"
-    >
+    <label {...api().getRootProps()}>
       <input {...api().getHiddenInputProps()} />
-      <span
-        {...api().getControlProps()}
-        class="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
-      >
-        <span
-          {...api().getThumbProps()}
-          class="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
-        />
+      <span {...api().getControlProps()}>
+        <span {...api().getThumbProps()} />
       </span>
     </label>
   )
