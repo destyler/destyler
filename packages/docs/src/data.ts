b8a17a08379ca172ef05d02fdb1267d59eb001f5
elonehoo <hi@elonehoo.me>
2025-10-05 23:47:37 +0800

    wip: 7

--- packages/docs/src/data.ts
+++ packages/docs/src/data.ts
@@ -1,7 +1,9 @@
+import accessibilityJsonData from '../data/accessibility.json'
 import apiJsonData from '../data/api.json'
 import dataAttrJsonData from '../data/data-attr.json'
 
 export const apiJson = apiJsonData
 export const dataAttrJson = dataAttrJsonData
+export const accessibilityJson = accessibilityJsonData
 
 export default apiJson
