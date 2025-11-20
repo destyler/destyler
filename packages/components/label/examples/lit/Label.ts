3d1987ca7ca8aae524b1600d692fb8deffdec604
elonehoo <hi@elonehoo.me>
2025-11-20 22:53:00 +0800

    feat: add label test and example

---
+++ packages/components/label/examples/lit/Label.ts
@@ -0,0 +1,36 @@
+import { MachineController, normalizeProps, spread } from '@destyler/lit'
+import '@destyler/shared-private/lit'
+import { html, LitElement } from 'lit'
+import { customElement } from 'lit/decorators.js'
+import * as label from '../../index'
+
+@customElement('destyler-label')
+export class LabelElement extends LitElement {
+  private machine = new MachineController(
+    this,
+    label.machine({
+      id: 'label:lit',
+    }),
+  )
+
+  render() {
+    const api = label.connect(this.machine.state, this.machine.send, normalizeProps)
+
+    return html`
+      <destyler-layout>
+        <label ${spread(api.getRootProps())}>
+          text
+        </label>
+        <destyler-toolbar>
+          <destyler-state-visualizer .state=${this.machine.state}></destyler-state-visualizer>
+        </destyler-toolbar>
+      </destyler-layout>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'destyler-label': LabelElement
+  }
+}
---
+++ packages/components/label/storybook/lit.stories.ts
@@ -0,0 +1,9 @@
+import Label from '../examples/lit/Label.astro'
+
+export default {
+  component: Label,
+}
+
+export const Default = {
+  args: {},
+}
