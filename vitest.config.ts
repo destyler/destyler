ad4dfccd6305538e25ded4599aa2b85da977fbac
elonehoo <hi@elonehoo.me>
2025-11-26 14:07:06 +0800

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import Utils from './vitest.utils.config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+    },
     projects: [
       Utils,
       Browser,
