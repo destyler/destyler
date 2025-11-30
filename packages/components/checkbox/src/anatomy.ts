3afc6fd73ba349b7f8ba2ee03ee0c0889e6c8d81
elonehoo <hi@elonehoo.me>
2025-11-30 22:18:37 +0800

    style: format anatomy parts for better readability

--- packages/components/checkbox/src/anatomy.ts
+++ packages/components/checkbox/src/anatomy.ts
@@ -1,4 +1,9 @@
 import { createAnatomy } from '@destyler/anatomy'
 
-export const anatomy = createAnatomy('checkbox').parts('root', 'label', 'control', 'indicator')
+export const anatomy = createAnatomy('checkbox').parts(
+  'root',
+  'label',
+  'control',
+  'indicator',
+)
 export const parts = anatomy.build()
