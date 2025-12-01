b4a879d3dd5533a3d1562093f3eab5b5db66724e
elonehoo <hi@elonehoo.me>
2025-12-01 17:46:19 +0800

    docs: add build layer

---
+++ document/src/data/build.ts
@@ -0,0 +1,15 @@
+export interface BuildData {
+  title: string
+  description: string
+  link: string
+  imgSrc: string
+}
+
+export const data: BuildData[] = [
+  {
+    title: 'Destyler UI',
+    description: 'Build scalable design systems with unstyled, accessible UI components',
+    link: 'https://github.com/destyler/ui',
+    imgSrc: 'https://elonehoo.me/destyler/node',
+  },
+]
