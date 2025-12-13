204fc59f669a6a89bd673048fe3d4205cf9ec1fd
elonehoo <hi@elonehoo.me>
2025-12-13 15:25:12 +0800

    feat: add DownloadTriggerProps and QrCodeGenerateResult types to qr-code exports

--- packages/components/qr-code/index.ts
+++ packages/components/qr-code/index.ts
@@ -5,9 +5,11 @@ export * from './src/props'
 export type {
   MachineApi as Api,
   UserDefinedContext as Context,
+  DownloadTriggerProps,
   ElementIds,
   MachineState,
   QrCodeGenerateOptions,
+  QrCodeGenerateResult,
   Send,
   Service,
   ValueChangeDetails,
